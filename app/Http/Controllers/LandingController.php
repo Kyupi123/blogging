46b8b5e69c148b570d3aac04c41a83f4e1715167
David Mangara <david.m.samosir@gmail.com>
2023-11-18 13:37:49 +0700

    Changes article cards into component-based

--- app/Http/Controllers/LandingController.php
+++ app/Http/Controllers/LandingController.php
@@ -14,6 +14,9 @@ class LandingController extends Controller
     {
         $articles = Article::query();
         if ($request->query('category')) {
+            //Filter artikel berdasarkan category
+            //select * from articles join categories on articles.category_id = categories.id
+            //where categories.slug = 'world-news'
             $articles->whereHas('category', function ($query) use ($request) {
                 $query->where('slug', $request->query('category'));
             });
